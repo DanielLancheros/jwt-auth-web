172ac515731423241665ef38e414dbc39bfe1dba
Daniel Lancheros <daniellancheros@Daniels-MacBook-Air.local>
2023-07-18 09:49:44 -0400

    added error

--- app/login/page.js
+++ app/login/page.js
@@ -1,5 +1,20 @@
+"use client"
+import { useState } from "react"
+
 export default function Login() {
-  return (
+
+  const [error, setError] = useState()
+
+  const handleLoginForm = (e) => {
+    e.preventDefault()
+    const email = e.target.email.value
+    const password = e.target.password.value
+    // now send email and password to api
+    // get back user and token
+    // store user and token
+    // handle any errors
+  }
+    return (
     <div className="w-full h-screen font-sans bg-cover bg-landscape">
       <div className="container flex items-center justify-center flex-1 h-full mx-auto">
         <div className="w-full max-w-lg">
@@ -12,7 +27,7 @@ export default function Login() {
                 <div className=" relative ">
                   <input
                     type="text"
-                    id="login-with-bg-email"
+                    id="email"
                     className=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
                     placeholder="email"
                   />
@@ -22,18 +37,23 @@ export default function Login() {
                 <div className=" relative ">
                   <input
                     type="text"
-                    id="login-with-bg-password"
+                    id="password"
                     className=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
                     placeholder="password"
                   />
                 </div>
               </div>
+              {error &&
+                <div className= "bg-red-200 border-red-600 text-red-600 border-1-4 p-4 mt-4" role="alert">
+                  <p className= "font-bold">Error</p>
+                  <p>{error}</p>
+                  </div>}
               <div className="flex items-center justify-between mt-4">
                 <button
                   type="submit"
                   className="py-2 px-4  bg-indigo-600 hover:bg-indigo-700 focus:ring-indigo-500 focus:ring-offset-indigo-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg "
                 >
-                  Validate
+                  Login
                 </button>
               </div>
               <div className="text-center">
