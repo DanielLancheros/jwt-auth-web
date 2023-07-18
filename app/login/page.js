803e7899a8993a673aa62daa5fbf6c98c843b780
Daniel Lancheros <daniellancheros@Daniels-MacBook-Air.local>
2023-07-18 10:54:43 -0400

    added AuthProvider around children

--- app/login/page.js
+++ app/login/page.js
@@ -10,16 +10,31 @@ export default function Login() {
     const email = e.target.email.value
     const password = e.target.password.value
     // now send email and password to api
+    fetch('http://127.0.0.1:5002/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ email, password }),
+    })
     // get back user and token
-    // store user and token
+    .then(res => res.json())
+    .then(data => {
+      if(data.message) {
+        setError(data.message)
+        return
+      }
+      // store user and token
+    })
     // handle any errors
+    .catch(error => setError(error.message))
   }
     return (
     <div className="w-full h-screen font-sans bg-cover bg-landscape">
       <div className="container flex items-center justify-center flex-1 h-full mx-auto">
         <div className="w-full max-w-lg">
           <div className="leading-loose">
-            <form className="max-w-sm p-10 m-auto rounded shadow-xl bg-white/25">
+            <form onSubmit={handleLoginForm} className="max-w-sm p-10 m-auto rounded shadow-xl bg-white/25">
               <p className="mb-8 text-2xl font-light text-center text-white">
                 Login
               </p>
@@ -36,7 +51,7 @@ export default function Login() {
               <div className="mb-2">
                 <div className=" relative ">
                   <input
-                    type="text"
+                    type="password"
                     id="password"
                     className=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
                     placeholder="password"
@@ -44,9 +59,8 @@ export default function Login() {
                 </div>
               </div>
               {error &&
-                <div className= "bg-red-200 border-red-600 text-red-600 border-1-4 p-4 mt-4" role="alert">
-                  <p className= "font-bold">Error</p>
-                  <p>{error}</p>
+                <div className= "bg-red-200 border-red-600 text-red-600 border-l-4 p-4 mt-4" role="alert">
+                  <p>❌ {error}</p>
                   </div>}
               <div className="flex items-center justify-between mt-4">
                 <button
